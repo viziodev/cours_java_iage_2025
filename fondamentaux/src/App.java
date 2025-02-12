7c140316bfa896d55d815f9708d1dc11cb91973d
douvewane <douvewane85@gmail.com>
2025-02-12 20:29:51 +0000

    Cours 1

--- fondamentaux/src/App.java
+++ fondamentaux/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Type primitifs
+         long nbre1;
+         double nbre2 ;//2.5
+    
+        //Decalration
+         Scanner scanner ;
+        //Instanciation
+        scanner = new Scanner(System.in);
+             System.out.println("Entrer un nombre1");
+             nbre1 = scanner.nextInt();
+              System.out.println("Entrer un nombre2");
+              nbre2 = scanner.nextDouble();
+              double som=nbre1+nbre2;
+             System.out.println("La somme est "+som);
+    
+        //Appel de methode
+        scanner.close();
+
+
     }
 }
